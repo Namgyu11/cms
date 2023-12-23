71128257ee3f0b29f1a4f6da4ba87c3fd828c543
Namgyu11 <hagaji3@naver.com>
2023-12-24 01:10:39 +0900

    서브 모듈화 작업

--- src/main/java/com/example/userapi/service/SignUpCustomerService.java
+++ src/main/java/com/example/userapi/service/SignUpCustomerService.java
@@ -13,7 +13,7 @@ public class SignUpCustomerService {
     private final CustomerRepository customerRepository;
 
     public Customer signUp(SignUpForm form){
-        // Customer 객체 만들기
+        // Customer 객체 만들기!!
        return this.customerRepository.save(Customer.from(form));
     }
 }
